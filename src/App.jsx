c15467e88fc5dc82b0aac22df37d04faafab4298
ianthehamster <ianchow9898@gmail.com>
2025-05-21 23:03:24 +0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import AudioFile from './assets/audio.mp3';
+import obiWanKenobiVoiceLines from './assets/obiWanKenobiVoiceLines.mp3';
 import AudioPlayer from './components/AudioPlayer';
 
 import './App.css';
 
 function App() {
-	return (
-		<>
-			<AudioPlayer audioFile={AudioFile} />
-		</>
-	);
+  return (
+    <>
+      <AudioPlayer audioFile={obiWanKenobiVoiceLines} />
+    </>
+  );
 }
 
 export default App;
